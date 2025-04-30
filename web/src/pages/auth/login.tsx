cd5f97ed90ee442244a344a54580d8d4faa4c38c
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2025-04-30 15:57:54 +0200

    log in and register pages and forms implemented - slight styles adjustments added

--- web/src/pages/auth/login.tsx
+++ web/src/pages/auth/login.tsx
@@ -2,10 +2,14 @@
 import PublicLayout from '../../components/PublicLayout';
 import LoginForm from './LoginForm';
 import Link from 'next/link';
+import { FiArrowLeft } from 'react-icons/fi';
 
 export default function LoginPage() {
   return (
     <PublicLayout>
+      <Link href="/" className="flex items-center text-black font-semibold mb-6 hover:underline">
+        <FiArrowLeft className="mr-2" /> Ritounin a kay
+      </Link>
       <div className="max-w-md mx-auto py-12">
         <h1 className="text-2xl font-bold mb-6">Konèkté kow'</h1>
         <LoginForm />
