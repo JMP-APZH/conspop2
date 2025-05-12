566642945c86913938b2b38e86fafde5349ea830
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2025-05-12 18:16:04 +0200

    connecting front to back - wrapping app with ApolloProvider

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,16 +1,14 @@
 import '../styles/globals.css';
-// export default function App({ Component, pageProps }) {
-//   return <Component {...pageProps} />;
-// }
 
 import { ApolloProvider } from '@apollo/client';
-import { useApollo } from '../lib/apollo-client';
+import { client } from '../lib/apollo-client';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: any) {
-  const apolloClient = useApollo(pageProps.initialApolloState);
+function MyApp({ Component, pageProps }: AppProps) {
+  // const apolloClient = useApollo(pageProps.initialApolloState);
   
   return (
-    <ApolloProvider client={apolloClient}>
+    <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
   );
