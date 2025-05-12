0612d9204e06bea1d6da144c58ea4496f9569ed3
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2025-05-12 18:13:12 +0200

    connecting front to back - apollo-client

--- web/src/lib/apollo-client.ts
+++ web/src/lib/apollo-client.ts
@@ -1,46 +1,24 @@
-// lib/apollo-client.ts
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { useMemo } from 'react';
 
-let client: ApolloClient<any>;
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql', // GraphQL endpoint
+});
 
-export function getApolloClient() {
-  if (!client) {
-    const httpLink = createHttpLink({
-      uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
-    });
-
-    const authLink = setContext((_, { headers }) => {
-      const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
-      return {
-        headers: {
-          ...headers,
-          authorization: token ? `Bearer ${token}` : "",
-        }
-      };
-    });
-
-    client = new ApolloClient({
-      link: authLink.concat(httpLink),
-      cache: new InMemoryCache(),
-      ssrMode: typeof window === 'undefined',
-    });
-  }
-  return client;
-}
-
-export function useApollo(initialState: any) {
-    const store = useMemo(() => initializeApollo(initialState), [initialState]);
-    return store;
-  }
+const authLink = setContext((_, { headers }) => {
+  // Get the authentication token from local storage if it exists
+  const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
   
-  function initializeApollo(initialState = null) {
-    const _apolloClient = getApolloClient();
-    
-    if (initialState) {
-      _apolloClient.cache.restore(initialState);
+  // Return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
     }
-    
-    return _apolloClient;
-  }
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
