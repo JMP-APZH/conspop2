698f0ba762e933e805666049026608d09421a3a8
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2025-05-15 06:49:35 +0200

    Admin dashboard setup - auth.ts types adjustments

--- backend/src/types/auth.ts
+++ backend/src/types/auth.ts
@@ -7,3 +7,23 @@ export interface RegisterInput {
     cityOfOrigin: string;
     currentCity: string;
   }
+
+  export enum UserRole {
+    USER = 'USER',
+    ADMIN = 'ADMIN'
+  }
+
+  export interface JwtPayload {
+    userId: string;
+    role: UserRole;
+    // possibility to add other claims that might be needed
+  }
+
+  export interface AuthUser {
+    id: string;
+    email: string;
+    role: UserRole;
+    firstName: string;
+    lastName: string;
+    // ... other user fields that might be needed in context
+  }
